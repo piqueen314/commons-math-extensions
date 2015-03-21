31a4ef959696df144fe03dc29680c5a97d617414
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2015-03-21 13:17:30 +0800

    ExtMath.fastExp is fixed to FastMath.exp from commons-math, documentation added

--- src/main/java/com/vsthost/rnd/commons/math/ext/linear/ExtMath.java
+++ src/main/java/com/vsthost/rnd/commons/math/ext/linear/ExtMath.java
@@ -16,19 +16,46 @@
 
 package com.vsthost.rnd.commons.math.ext.linear;
 
+import org.apache.commons.math3.util.FastMath;
+
 /**
- * Created by vst on 6/3/15.
+ * Provides extensions to standard math functions.
  */
 public class ExtMath {
+    
+    /**
+     * Returns a fast, nearly accurate implementation of exponential function.
+     *
+     * <p>Note that the implementation is not yet settled. There are faster or more accurate ways to do it.</p>
+     *
+     * @param x A double value to be provided
+     * @return The e^x as a double value.
+     */
     public static double fastExp(double x) {
-        // TODO: Find the most efficient and accurate exp method.
-        x = 1d + x / 256d;
-        x *= x; x *= x; x *= x; x *= x;
-        x *= x; x *= x; x *= x; x *= x;
-        return x;
-        //return Math.exp(x);
-        //return FastMath.exp(x);
-        //return Double.longBitsToDouble(((long) (1512775 * val + 1072632447)) << 32);
-        //return Double.longBitsToDouble(((long) (1512775 * val + (1072693248 - 60801))) << 32);
+        return FastMath.exp(x);
+
+        // Below, we have other ways of achieving this:
+        //
+        // Method 1:
+        // =========
+        // x = 1d + x / 256d;
+        // x *= x; x *= x; x *= x; x *= x;
+        // x *= x; x *= x; x *= x; x *= x;
+        // return x;
+        //
+        // Method 2:
+        // =========
+        //
+        // return Math.exp(x);
+        //
+        // Method 3:
+        // =========
+        //
+        // return Double.longBitsToDouble(((long) (1512775 * val + 1072632447)) << 32);
+        //
+        // Method 4:
+        // =========
+        //
+        // return Double.longBitsToDouble(((long) (1512775 * val + (1072693248 - 60801))) << 32);
     }
 }
