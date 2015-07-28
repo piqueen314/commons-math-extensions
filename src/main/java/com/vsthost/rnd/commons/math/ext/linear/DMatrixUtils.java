9cf12f038410b682da2889b1e1df9d8be7639ccf
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2015-07-28 14:40:36 +0800

    Added Zero-Mean Based Distribution functionality, related helpers

--- src/main/java/com/vsthost/rnd/commons/math/ext/linear/DMatrixUtils.java
+++ src/main/java/com/vsthost/rnd/commons/math/ext/linear/DMatrixUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Vehbi Sinan Tunalioglu <vst@vsthost.com>.
+ * Copyright (c) 2015 Vehbi Sinan Tunalioglu <vst@vsthost.com>, Tolga Sezer <tolgasbox@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,11 @@
 package com.vsthost.rnd.commons.math.ext.linear;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.distribution.UniformRealDistribution;
 import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.util.MathArrays;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -30,6 +34,8 @@ import java.util.stream.IntStream;
 /**
  * This class provides utilities for matrices implemented as
  * ordinary arrays of double arrays.
+ *
+ * @author Vehbi Sinan Tunalioglu, Tolga Sezer
  */
 public class DMatrixUtils {
     /**
@@ -251,6 +257,54 @@ public class DMatrixUtils {
     }
 
     /**
+     * Get the order of the specified elements in descending or ascending order.
+     *
+     * @param values A vector of integer values.
+     * @param indices The indices which will be considered for ordering.
+     * @param descending Flag indicating if we go descending or not.
+     * @return A vector of indices sorted in the provided order.
+     */
+    public static int[] getOrder(int[] values, int[] indices, boolean descending) {
+        // Create an index series:
+        Integer[] opIndices = ArrayUtils.toObject(indices);
+
+        // Sort indices:
+        Arrays.sort(opIndices, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (descending) {
+                    return Double.compare(values[o2], values[o1]);
+                } else {
+                    return Double.compare(values[o1], values[o2]);
+                }
+            }
+        });
+
+        return ArrayUtils.toPrimitive(opIndices);
+    }
+
+    /**
+     * Get the order of the elements in descending or ascending order.
+     *
+     * @param values A vector of integer values.
+     * @param descending Flag indicating if we go descending or not.
+     * @return A vector of indices sorted in the provided order.
+     */
+    public static int[] getOrder(int[] values, boolean descending) {
+        return DMatrixUtils.getOrder(values, IntStream.range(0, values.length).toArray(), descending);
+    }
+
+    /**
+     * Get the order of the elements in ascending order.
+     *
+     * @param values A vector of integer values.
+     * @return A vector of indices sorted in the ascending order.
+     */
+    public static int[] getOrder(int[] values) {
+        return DMatrixUtils.getOrder(values, false);
+    }
+
+    /**
      * Returns the DOWN rounded value of the given value for the given steps.
      *
      * @param value The original value to be rounded.
@@ -301,5 +355,202 @@ public class DMatrixUtils {
             return down;
         }
         return up;
+    }
+
+    /**
+     * Creates a matrix from the provided range for columns with the given number of rows.
+     *
+     * @param lower The lower bound (inclusive) for the range.
+     * @param upper The upper bound (exclusive) for the range.
+     * @param rows The number of rows.
+     * @return A new matrix.
+     */
+    public static double[][] matrixFromRange (double[] lower, double[] upper, int rows) {
+        // Get the number of columns:
+        final int cols = lower.length;
+
+        // Initialize the return value:
+        final double[][] matrix = new double[rows][cols];
+
+        // Iterate over columns and fill the array:
+        for (int col = 0; col < cols; col++) {
+            // Get the sequence:
+            final double[] sequence = DMatrixUtils.sequence(lower[col], upper[col], rows);
+
+            // Assign to each row in the column:
+            for (int row = 0; row < sequence.length; row++) {
+                matrix[row][col] = sequence[row];
+            }
+        }
+
+        // Done, return the populated matrix:
+        return matrix;
+    }
+
+    /**
+     * Computes the cumulative sums of a given vector.
+     *
+     * @param vector Vector which the cumulative sums to be computed of.
+     * @return Cumulative sums of the vector.
+     */
+    public static double[] cumsum (double[] vector) {
+        // Initialize the return value:
+        final double[] retval = new double[vector.length];
+
+        // Iterate and cumulate:
+        retval[0] = vector[0];
+        for (int i = 1; i < retval.length; i++) {
+            retval[i] = vector[i] + retval[i - 1];
+        }
+
+        // Done, return:
+        return retval;
+    }
+
+    /**
+     * Provides a workhorse function for {@link DMatrixUtils#zmdb} by shuffling indices for operation.
+     *
+     * @param lower Lower distances.
+     * @param upper Upper distances
+     * @return The zero-mean bounded uniform distribution.
+     */
+    private static double[] _zmdbInner (double[] lower, double[] upper, RandomGenerator randomGenerator) {
+        // Define the dimension of the problem:
+        final int dimension = lower.length;
+
+        // Define the return value:
+        final double[] values = new double[dimension];
+
+        // Get the sum of lowers and uppers:
+        final double lowerSum = StatUtils.sum(lower);
+        final double upperSum = StatUtils.sum(upper);
+
+        // Initialize the contingencies to cumulative sums:
+        final double[] contingencyLower = DMatrixUtils.cumsum(lower);
+        final double[] contingencyUpper = DMatrixUtils.cumsum(upper);
+
+        // Iterate over contingencies and update:
+        for (int i = 0; i < dimension; i++) {
+            contingencyLower[i] = lowerSum - contingencyLower[i];
+            contingencyUpper[i] = upperSum - contingencyUpper[i];
+        }
+
+        // Define the mean value to be updated:
+        double mean = 0.0;
+
+        // Iterate over the dimension in a random order and update permissible draws and mean value:
+        double minValue, maxValue;
+        for (int index = 0; index < dimension - 1; index++) {
+            // Define the min/max value:
+            minValue = lower[index];
+            maxValue = upper[index];
+
+            // Simulate or get anthitetical for the element:
+            if (mean == 0.0) {
+                // Update the effective range:
+                minValue = Math.max(minValue, -contingencyUpper[index]);
+                maxValue = Math.min(maxValue, -contingencyLower[index]);
+
+                if (minValue == maxValue) {
+                    values[index] = minValue;
+                }
+                else {
+                    values[index] = new UniformRealDistribution(randomGenerator, Math.min(minValue, maxValue), Math.max(minValue, maxValue)).sample();
+                }
+            }
+            else if (mean > 0.0) {
+                values[index] = Math.max(-mean, minValue);
+            }
+            else {
+                values[index] = Math.min(-mean, maxValue);
+            }
+
+            // Update the mean:
+            mean += values[index];
+        }
+
+        // Fix the last element:
+        values[dimension - 1] = -mean;
+
+        // Done, return with pride!
+        return values;
+    }
+
+    /**
+     * Computes the zero-mean bounded distribution for the provided distances.
+     *
+     * @param lower Lower distances.
+     * @param upper Upper distances
+     * @return The zero-mean bounded uniform distribution.
+     */
+    public static double[] zmdb (double[] lower, double[] upper, RandomGenerator randomGenerator) {
+        // Check dimension match:
+        if (lower.length != upper.length) {
+            throw new IllegalArgumentException("Lower and upper bounds must be of same length.");
+        }
+
+        // Check for positive lower bounds which is not what we expect:
+        for (int i = 0; i < lower.length; i++) {
+            if (lower[i] > 0.0) {
+                throw new IllegalArgumentException("Lower bounds must be equal to or less than 0.");
+            }
+        }
+
+        // Check for positive lower bounds which is not what we expect:
+        for (int i = 0; i < upper.length; i++) {
+            if (upper[i] < 0.0) {
+                throw new IllegalArgumentException("Upper bounds must be equal to or greater than 0.");
+            }
+        }
+
+        // Get indices and shuffle:
+        final int[] indices = DMatrixUtils.shuffleIndices(lower.length, randomGenerator);
+
+        // Call auxiliary function:
+        final double[] zmbdValues = DMatrixUtils._zmdbInner(
+                DMatrixUtils.applyIndices(lower, indices),
+                DMatrixUtils.applyIndices(upper, indices),
+                randomGenerator);
+
+        // Reapply original indices and return:
+        return DMatrixUtils.applyIndices(zmbdValues, DMatrixUtils.getOrder(indices));
+    }
+
+    /**
+     * Consumes the length of an array and returns its shuffled indices.
+     *
+     * @param length The length of the arrays of which indices to be shuffled.
+     * @param randomGenerator Random number generator.
+     * @return Shuffled indices.
+     */
+    public static int[] shuffleIndices (int length, RandomGenerator randomGenerator) {
+        // Initialize indices:
+        final int[] indices = IntStream.range(0, length).toArray();
+
+        // Shuffle the array:
+        MathArrays.shuffle(indices, randomGenerator);
+
+        // Done return shuffled indices:
+        return indices;
+    }
+
+    /**
+     * Consumes an array and desired respective indices in an array and return a new array with values from the desired indices.
+     *
+     * @param vector Values.
+     * @param indices Desired indices for order.
+     * @return A new array.
+     */
+    public static double[] applyIndices (double[] vector, int[] indices) {
+        // Initialize the return array:
+        final double[] retval = new double[vector.length];
+
+        // Iterate over indices and populate:
+        for (int i = 0; i < retval.length; i++) {
+            retval[i] = vector[indices[i]];
+        }
+
+        // Done, return the return value:
+        return retval;
     }
 }
